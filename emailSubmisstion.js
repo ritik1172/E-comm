7bceb9d89636890c7dbc2d6cd56a009d1885e140
RitikDwivedi <ritikcu@gmail.com>
2025-03-23 11:56:21 +0530

    initial

---
+++ emailSubmisstion.js
@@ -0,0 +1,12 @@
+// document.getElementById("btn").addEventListener("click" , () => {
+//     const selectedElement = document.getElementById("selection");
+//     const selectedValue = selectedElement.value
+//     console.log(selectedValue)
+// });
+// console.log("Hello");
+
+window.onload(() => {
+    document.getElementById("name").value = ""
+    document.getElementById("phone").value = ""
+    document.getElementById("city").value = ""
+})
